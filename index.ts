5cab7f1d0c2f2cc06d82c9235a79f117f52b6993
marcoak11 <31699441+marcoak11@users.noreply.github.com>
2023-10-13 09:29:08 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,26 +5,42 @@ import { Server, Socket } from "socket.io";
 import { ExtendedError } from 'socket.io/dist/namespace';
 import cors from 'cors';
 
+ 
+
 const app = express();
 
+ 
+
 app.use(cors());
+
+ 
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 const server = createServer(app);
 
+ 
+
 //Socket.IO Server
 interface ServerToClientEvents { //listen
-    status: (payload: string) => void; //dal middleware
-    abort: () => void; //da mobile
+    status: (payload: string) => void;
+    abort: () => void;
 }
+
+ 
 
 type ClientToServerEvents = ServerToClientEvents //emit
 
+ 
+
 interface InterServerEvents { }
 
+ 
+
 interface SocketData { }
+
+ 
 
 const socketIO = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server, {
     cors: {
@@ -32,30 +48,46 @@ const socketIO = new Server<ClientToServerEvents, ServerToClientEvents, InterSer
     },
 });
 
+ 
+
 const middlewaresSockets = socketIO.of("/middlewares");
 const clientsSockets = socketIO.of("/clients");
+
+ 
 
 //middlewares
 const authorizationMiddleware = (socket: Socket<ServerToClientEvents, ServerToClientEvents, InterServerEvents, SocketData>, next: (err?: ExtendedError | undefined) => void) => {
     if (socket.handshake.query["type"] === undefined)
         next(new Error("Client type not defined"));
 
+ 
+
     next()//verifica che il type sia definito nella query
 }
 
+ 
+
 socketIO.use(authorizationMiddleware);
-middlewaresSockets.use(authorizationMiddleware)
-clientsSockets.use(authorizationMiddleware)
+socketIO.of("/middlewares").use(authorizationMiddleware)
+socketIO.of("/clients").use(authorizationMiddleware)
+
+ 
 
 middlewaresSockets.on('connection', (socket: Socket) => {
     console.log(socket.handshake.auth)
+
+ 
 
     socket.on("status", (payload: string) => {
         clientsSockets.emit("status", payload)
     })
 
+ 
+
     console.log("CLIENT CONNECTED middlewares", socket.id)
 })
+
+ 
 
 clientsSockets.on('connection', (socket: Socket) => {
     socket.on("abort", () => {
@@ -63,6 +95,8 @@ clientsSockets.on('connection', (socket: Socket) => {
     })
     console.log("CLIENT CONNECTED clients", socket.id)
 })
+
+ 
 
 const port = process.env.SERVER_LISTENING_PORT ? process.env.SERVER_LISTENING_PORT : "8080"
 server.listen(port, () => { console.log("Listening on port", port) });
